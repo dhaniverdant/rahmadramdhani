2a6b005b1dc49cf58041f484fd0290e0eb61b050
dhaniverdant <dhaniverdant23@gmail.com>
2022-08-19 22:57:18 +0800

    create and styling landing items

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import "../landing.css";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className='container landing-wrapper d-flex flex-column justify-content-center'>
+      <div className='welcome'>Welcome.</div>
+      <div>My name is Rahmad Ramdhani. I am a Front-End Developer based in Sumbawa, Indonesia.</div>
+    </div>
   );
 }
 
