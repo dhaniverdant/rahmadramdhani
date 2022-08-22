994bee738d215e75670a03591e89266a8f4aea83
dhaniverdant <dhaniverdant23@gmail.com>
2022-08-22 23:19:08 +0800

    add text on first section

--- src/pages/Inv.jsx
+++ src/pages/Inv.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import '../inv.css';
+import downArrow from '../icons/down-arrow.png';
 
 const Inv = () => {
   return (
     <div>
       <div id='landing' className='inv-landing'>
-        landing page
-        <a href="#second">Go To Middle</a>  
+        <div className='intro-title'>The Wedding Of</div>
+        <div className='names'>Messi & Antonella</div>
+        <div>Minggu, 31 Desember 2022</div>
+        <div className='to-title'>
+          <div>Kepada Yth.</div>
+          <div>Bapak/Ibu/Saudara/i</div>
+          <div className='attendant-name'>Neymar Jr.</div>
+        </div>
+        <a href="#second">
+          <img src={downArrow} className='down-arrow' alt="" />
+        </a>  
       </div>
       <div id='second' className='inv-second'>page 2</div>
     </div>
