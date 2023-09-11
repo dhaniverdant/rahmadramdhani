ced8eacee96d7f844e8b1279a826ba0d558c821f
dhaniverdant <dhaniverdant23@gmail.com>
2023-09-11 08:57:59 +0800

    tidy up and add linkedin icon

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import "../landing.css";
+import linkedin from '../icons/icons8-linkedin-64.png';
 
 const Landing = () => {
   return (
-    <div className='container landing-wrapper d-flex flex-column justify-content-center'>
-      <div className='welcome'>Welcome.</div>
-      <div>My name is Rahmad Ramdhani. I am a Front-End Developer based in Sumbawa, Indonesia.</div>
+    <div className='landing-wrapper d-flex flex-column justify-content-center'>
+      <div className="container">
+        <div className='welcome'>Hi There!</div>
+        <div>My name is Rahmad Ramdhani. I am a Front-End Developer based in Sumbawa, Indonesia.</div>
+        <div className="linkedin-icon-wrapper">
+          <img src={linkedin} alt='linkedin' />
+        </div>
+      </div>
     </div>
   );
 }
